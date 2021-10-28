dd438a111d938416d4923f76696faf71a6482669
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-10-27 21:03:36 -0300

    criada pastas para desenvolvimento

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+import app from './app';
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
+
+app.listen(port, () => {
+  console.log('Servidor Ativo 🎉🎉');
+});
