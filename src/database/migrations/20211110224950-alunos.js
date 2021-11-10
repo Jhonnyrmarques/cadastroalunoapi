98170b22150c5c3aefc7191f53d9449ea9c15b34
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-11-10 20:06:49 -0300

    criada a tabela de alunos

---
+++ src/database/migrations/20211110224950-alunos.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('alunos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ra: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('alunos');
+  },
+};
