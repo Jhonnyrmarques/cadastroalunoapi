1549637a288cf998c35751387dd1765aa2be9f62
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-11-24 19:18:05 -0300

    criada model de alunos e disciplinas

---
+++ src/models/Aluno.js
@@ -0,0 +1,38 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Aluno extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: ' ',
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: ' ',
+      },
+      ra: {
+        type: Sequelize.INTEGER,
+        defaultValue: ' ',
+        unique: {
+          msg: 'RA já cadastrado na base de dados',
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'E-mail já cadastrado na base de dados',
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.Disciplina, { foreignKey: 'aluno_id' });
+  }
+}
