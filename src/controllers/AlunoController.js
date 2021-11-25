22894d6c205228fd375b4a3e5e3182d1a087979a
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-11-24 22:22:04 -0300

    desenvolvido cadatro de alunos no controller

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,6 +1,21 @@
+import Aluno from '../models/Aluno';
+
 class AlunoController {
-  index(req, res) {
-    return res.json('ok');
+  async index(req, res) {
+    const alunos = await Aluno.findAll();
+    console.log(alunos);
+    res.json(alunos);
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
   }
 }
 
