34ce98460eb2710ff907b304c65161f46446b23e
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-11-24 22:18:41 -0300

    criada a rota post de aluno

--- src/routes/alunos.js
+++ src/routes/alunos.js
@@ -4,5 +4,6 @@ import AlunoController from '../controllers/AlunoController';
 const routes = new Router();
 
 routes.get('/', AlunoController.index);
+routes.post('/', AlunoController.store);
 
 export default routes;
