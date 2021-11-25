0a0c43ca4eb276d88ccd30fea5d721ce1f4db0d2
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-11-25 20:56:36 -0300

    criada rotas de atualização e exclusão de alunos

--- src/routes/alunos.js
+++ src/routes/alunos.js
@@ -5,5 +5,8 @@ const routes = new Router();
 
 routes.get('/', AlunoController.index);
 routes.post('/', AlunoController.store);
+routes.put('/:id', AlunoController.update);
+routes.get('/:id', AlunoController.show);
+routes.delete('/:id', AlunoController.delete);
 
 export default routes;
